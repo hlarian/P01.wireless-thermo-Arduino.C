0bff2238dc92a9a1f7b4eba3208699fb765b101f
Habib Larian <36689969+hlarian@users.noreply.github.com>
2018-05-07 00:02:04 +0430

    Add files via upload

---
+++ ESP8266/dw/dw.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "skynet";
+const char* password = "yda32+101110001";
+
+// Create an instance of the server
+// specify the port to listen on (first argument)
+WiFiServer server(8080);  
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);   
+  // Start the server
+  server.begin();
+  
+  Serial.print(WiFi.localIP());
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+
+  // Wait until the client sends some data
+    if(!client){
+    delay(1);
+  }   
+  
+   String temp=Serial.readStringUntil('X');    
+   client.println(temp);
+  
+  delay(200);
+}
+
---
+++ dht11/dht/dht.ino
@@ -0,0 +1,27 @@
+#include <dht.h>
+
+dht DHT;
+
+void setup(){
+  Serial.begin(115200);
+  delay(10);
+}
+
+void loop()
+{
+  int chk = DHT.read11(7);
+ Serial.print("Temp:  ");
+ Serial.print(DHT.temperature);
+ Serial.print(" C");
+ Serial.print("       ");
+ Serial.print("humidity:  ");
+ Serial.print(DHT.humidity);
+ Serial.print(" %");
+ Serial.print("X");
+// 
+
+
+   
+            
+  delay(1000);
+}
